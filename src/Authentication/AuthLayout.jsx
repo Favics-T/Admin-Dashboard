ed4c72df5f7b5f4e2dc4c13a9e6c76f999a43f49
unknown <taiwokolapo533@gmail.com>
2025-08-07 09:09:19 +0100

    working on responsiveness

--- src/Authentication/AuthLayout.jsx
+++ src/Authentication/AuthLayout.jsx
@@ -4,11 +4,11 @@ import SplashCard from './components/SplashCard'
 const AuthLayout = ({ children }) => {
   return (
     <div className='  h-screen '>
-      <div className='flex h-full'>
-       <div className='w-1/2'>
+      <div className='flex md:flex-row flex-col h-full'>
+       <div className='md:w-1/2'>
 <SplashCard />
        </div>
-       <div className='w-1/2'>
+       <div className='md:w-1/2'>
         {children}
        </div>
         
