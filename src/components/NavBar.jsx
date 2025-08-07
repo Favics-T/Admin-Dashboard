0bbc0bf8fd879697bf028a2e8f5734e9a75a52d5
unknown <taiwokolapo533@gmail.com>
2025-08-07 23:05:59 +0100

    checking..

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom';
+import { LuMenu } from "react-icons/lu";
+
 
 const routeTitleMap = {
   '/dashboard': 'Dashboard',
@@ -14,10 +16,12 @@ const NavBar = () => {
 
   const title = routeTitleMap[pathname] || 'Dashboard';
   return (
-    <div className=' md:pt-8 pt-2 flex md:gap-20 gap-4 items-center'>
+    <div className=' md:pt-8 pt-2 md:justify-start justify-between flex md:gap-20 gap-4 items-center'>
     <h1 className='text-[#6E39CB] Lato md:text-2xl'> {title} </h1>  
       <input type="text" placeholder='Search anything here'  
-      className='bg-white md:p-2 text-[#DBDCDE] border p-1 rounded-full md:w-96'/>
+      className='bg-white md:block hidden md:p-2 text-[#DBDCDE] border p-1 rounded-full md:w-96'/>
+<LuMenu className='md:hidden block'/>
+
     </div>
   )
 }
