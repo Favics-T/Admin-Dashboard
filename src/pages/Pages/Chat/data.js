394ab20baf578328d8f3c9a936d12d924be5ab1e
unknown <taiwokolapo533@gmail.com>
2025-08-24 09:58:48 +0100

    working on chat page, created data file to manage code

---
+++ src/pages/Pages/Chat/data.js
@@ -0,0 +1,121 @@
+export const AVATAR_COLORS = {
+  green: "bg-emerald-500",
+  purple: "bg-violet-500",
+  orange: "bg-amber-500",
+  pink: "bg-pink-500",
+  blue: "bg-sky-500",
+};
+
+export const people = [
+{
+    id: 1,
+    name: "Jacob Jones",
+    subtitle: "Lorum ipsum dolor sit...",
+    time: "5m",
+    color: "green" 
+},
+
+{
+     id: 2,
+     name: "Leslie Alexander",
+     subtitle: "Lorum ipsum dolor sit...",
+     time: "5m",
+     color: "orange" 
+},
+
+ 
+{
+     id: 3,
+     name: "Eleanor Pena",
+     subtitle: "Lorum ipsum dolor sit...",
+     time: "5m",
+     color: "purple" 
+},
+
+{
+     id: 4,
+     name: "Kathryn Murphy",
+     subtitle: "Lorum ipsum dolor sit...",
+     time: "5m",
+     color: "pink" 
+},
+
+  
+{
+     id: 5,
+     name: "Wade Warren",
+     subtitle: "Lorum ipsum dolor sit...",
+     time: "5m",
+     color: "blue" 
+    
+},
+
+{
+     id: 6,
+     name: "Eleanor McKinney",
+     subtitle: "Meeting request",
+     time: "5m",
+     color: "purple" 
+},
+
+{
+     id: 7,
+     name: "Jacob Jones",
+     subtitle: "Lorum ipsum dolor sit...",
+     time: "5m", color: "green" 
+},
+
+  
+{
+ id: 8,
+ name: "Marvin McKinney",
+ subtitle: "Hiring assistant",
+ time: "5m",
+ color: "pink"
+},
+
+];
+
+export  const sampleMessages = [
+  {
+    id: "m1",
+    side: "in",
+    type: "text",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dolor mollis eu gravida habitant. Tellus donec id. Fermentum suspendisse. Pharetra id nisl a massa.",
+    time: "10:15 pm",
+  },
+
+{ 
+    id: "m2",
+    side: "out",
+    type: "voice",
+    time: "10:25 pm",
+    duration: "0:32" 
+},
+
+{
+    id: "m3",
+    side: "in",
+    type: "voice",
+    time: "10:26 pm",
+    duration: "0:12" 
+},
+
+  {
+    id: "m4",
+    side: "out",
+    type: "text",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dolor mollis eu gravida habitant odio non. Tellus donec id nisl.",
+    time: "10:35 pm",
+  },
+
+  {
+     id: "m5",
+     side: "in",
+     type: "text",
+     text: "Got you.",
+     time: "10:55 pm"
+ },
+];
