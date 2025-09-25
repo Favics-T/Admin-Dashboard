c26b30b57381e1ad9eaec2c7cf483574ef3d8424
unknown <taiwokolapo533@gmail.com>
2025-09-25 06:50:06 +0100

    working on kanban

---
+++ src/pages/Application/KanbanComponents/CardBody.jsx
@@ -0,0 +1,29 @@
+import { div } from 'framer-motion/client'
+import React from 'react'
+
+const CardBody = ({children,type}) => {
+    return(
+        <div className='border shadow-xl border-gray-200 rounded h-full'>
+            <div className='p-2 bg-[#e1dde7] border-t-2 border-[#6E39CB] flex justify-between'>
+                <h1>{type}</h1>
+                <h1>-</h1>
+            </div>
+            
+            <div className='px-6 py-2'>
+ {
+            type === 'To Do Task'
+            ?
+            (<div className='border-2 p-2 rounded flex items-center text-[#89868D] justify-center border-dotted '>
+              <h1>+</h1>  </div>):
+                <div></div>
+           } 
+
+            <div>{children}</div>
+            </div>
+
+          
+        </div>
+    )
+}
+
+export default CardBody
--- src/pages/Application/KanbanComponents/KanbanBody.jsx
+++ src/pages/Application/KanbanComponents/KanbanBody.jsx
@@ -5,15 +5,18 @@ import KanbanComponent from './KanbanComponent'
 import ToDoTask from './ToDoTask'
 import InProgress1 from './InProgress1'
 import InProgress2 from './InProgress2'
+import CardBody from './CardBody'
 
 const KanbanBody = () => {
 
   return (
     <div>
-      <div className='border grid grid-cols-3   border-gray-200 p-1 rounded-lg'>
-      <ToDoTask />
-      <InProgress1 />
-      <InProgress2 />
+      <div className='border grid grid-cols-3 gap-2   border-gray-200 p-1 rounded-lg'>
+     {
+      kanbanData.map(({type})=>(
+        <CardBody type={type}/>
+      ))
+     }
       
       </div>
     </div>
