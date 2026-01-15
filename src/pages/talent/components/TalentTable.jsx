d207f375e5eeb48982b2eab468a8fc74c7fc64a7
unknown <taiwokolapo533@gmail.com>
2026-01-15 12:18:08 +0100

    tested and ensured my mock api works fine, after which i fetched talents

---
+++ src/pages/talent/components/TalentTable.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TalentTable = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TalentTable
---
+++ src/pages/talent/page/Talent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Talent() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Talent
